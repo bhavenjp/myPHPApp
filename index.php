3066a17f9c75a52b069238797338dd04a1342e44
root <root@ProdServer.vlab.local>
2017-08-07 21:13:58 +0800

    v0.25

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<H2>Hello World - v0.2</H2>'; ?> 
+ <?php 
+	echo '<H2>Hello GIC - v0.2 </H2>'; 
+	$dtz = new DateTimeZone("Europe/Madrid"); //Your timezone
+	$now = new DateTime(date("Y-m-d H:i:s"), $dtz);
+	echo $now->format("Y-m-d H:i:s");
+ ?> 
  </body>
 </html>
